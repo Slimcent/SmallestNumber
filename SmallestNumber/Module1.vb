02733c21b390ff9d19d6e6f0870423c1b2bfce8a
Slimcent <33299753+Slimcent@users.noreply.github.com>
2021-10-27 13:00:19 +0100

    All files

---
+++ SmallestNumber/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim number1 As Integer = InputBox("")
+        Dim number2 As Integer = InputBox("")
+        Dim number3 As Integer = InputBox("")
+
+        If number1 < number2 AndAlso number1 < number3 Then
+            number1 = Integer.Parse(Console.ReadLine())
+            MsgBox($"The Smallest number is {number1}")
+        ElseIf number2 < number1 AndAlso number2 < number3 Then
+            number2 = Integer.Parse(Console.ReadLine())
+            MsgBox($"The Smallest number is {number2}")
+        ElseIf number3 < number1 AndAlso number3 < number2 Then
+            number3 = Integer.Parse(Console.ReadLine())
+            MsgBox($"The Smallest number is {number3}")
+        End If
+
+    End Sub
+
+End Module
